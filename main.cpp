573032500aac63495dccc3877b38e484d1cc2db2
David Payne <100661623+dpayne77@users.noreply.github.com>
2023-04-17 16:26:11 -0400

    Prompting user for search type and sort type

--- main.cpp
+++ main.cpp
@@ -3,8 +3,61 @@
 #include "movies.h"
 #include "quicksort.h"
 #include "mergesort.h"
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Welcome to Movie Maestro!" << endl;
+    // 1. do parsing with parse.h
+
+    // 2. Prompt user with search options
+    string userInput;
+    userInput = "";
+
+    while (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4") { // will loop if user does not input 1-4
+        cout << "How would you like to search?" << endl;
+        cout << "1) Search by actor" << endl;
+        cout << "2) Search by director" << endl;
+        cout << "3) Search by genre" << endl;
+        cout << "4) Search by gross box office" << endl;
+        cout << "Enter a number 1-4:" << endl;
+
+        cin >> userInput;
+
+        if (userInput == "1") {
+            cout << "What actor would you like to search for?" << endl;
+        }
+        else if (userInput == "2") {
+            cout << "What director would you like to search for?" << endl;
+        }
+        else if (userInput == "3") {
+            cout << "What genre would you like to search for?" << endl;
+            // maybe print options later?
+        }
+        else if (userInput == "4") {
+            // Option 4 (box office) doesn't need a secondary question
+        }
+        else {
+            cout << "Please enter 1-4." << endl;
+        }
+
+    }
+    userInput = ""; // resetting userInput
+
+    // 3. Prompt user with sort options
+    while (userInput != "1" && userInput != "2") { // will loop if user does not input 1-2
+        cout << "Select a sorting algorithm" << endl;
+        cout << "1) Merge Sort" << endl;
+        cout << "2) Quick Sort" << endl;
+        cout << "Enter a number 1-2:" << endl;
+
+        cin >> userInput;
+
+        // check for input errors
+        if (userInput != "1" && userInput != "2") {
+            cout << "Please enter 1 or 2." << endl;
+        }
+    }
 }
