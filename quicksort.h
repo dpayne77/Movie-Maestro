bdd021cb9128cddfa832b2afa9ac561bc180a139
dpayne77 <100661623+dpayne77@users.noreply.github.com>
2023-04-17 16:44:09 -0400

    Made quicksort

--- quicksort.h
+++ quicksort.h
@@ -1,1 +1,38 @@
 // quick sort will go here
+#include "movies.h"
+#include <iostream>
+
+using namespace std;
+
+void quickSort(int ratings[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(ratings, low, high);
+        quickSort(ratings, low, pivot -1);
+        quickSort(ratings, pivot + 1, high);
+    }
+}
+
+int partition(int ratings[], int low, int high) {
+    int pivot = ratings[low];
+    int up = low;
+    int down = high;
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (ratings[up] > pivot) {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--) {
+            if (arr[down] < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(ratings[up], ratings[down]);
+        }
+    }
+    swap(ratings[low], ratings[down]);
+    return down;
+}
