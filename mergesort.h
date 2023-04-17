ebc6c31b685c55526bb65a52f9813fc95213fe71
cjannun <93235878+cjannun@users.noreply.github.com>
2023-04-17 16:42:00 -0400

    Finished MergeSort skeleton

--- mergesort.h
+++ mergesort.h
@@ -23,10 +23,24 @@ void merge(int arr[], int leftIndex, int midIndex, int rightIndex) {
         leftHalf[i] = arr[leftIndex + 1]
     }
     for (int i = 0; i < rightSize; i++) {
-        rightHalf[i] = arr[mid + i + 1]
+        rightHalf[i] = arr[midIndex + i + 1]
     }
 
-    int left
+    int leftCt = 0;
+    int rightCt = 0;
+    int mergedCt = leftIndex;
 
-
+    while (leftCt < leftSize && rightCt < rightSize) {
+        if (leftHalf[leftCt] <= rightHalf[rightCt]) {
+            arr[mergedCt++] = leftHalf[leftCt++];
+        } else {
+            arr[mergedCt++] = rightHalf[rightCt++];
+        }
+        while (leftCt < leftSize) {
+            arr[mergedCt++] = leftHalf[leftCt++];
+        }
+        while (rightCt < rightSize) {
+            arr[mergedCt++] = rightHalf[rightCt++];
+        }
+    }
 }
