b1c64eb92c8038d96bcaf805e5c509e8aa29af7f
cjannun <93235878+cjannun@users.noreply.github.com>
2023-04-18 10:58:52 -0400

    Started linking mergesort to Movies

--- mergesort.h
+++ mergesort.h
@@ -7,7 +7,7 @@ using namespace std;
 void mergeSort(int ratings[], int start, int end);
 void merge(int arr[], int leftIndex, int midIndex, int rightIndex);
 
-void mergeSort(int ratings[], int start, int end) {
+void mergeSort(Movies ratings[], int start, int end) {
     // source: Discussion 8 - Sorting slides, slide 23
   if (start < end) {
       int middle = (start + end) / 2;
@@ -17,7 +17,7 @@ void mergeSort(int ratings[], int start, int end) {
   }
 }
 
-void merge(int arr[], int leftIndex, int midIndex, int rightIndex) {
+void merge(Movies arr[], int leftIndex, int midIndex, int rightIndex) {
     // source: Discussion 8 - Sorting slides, slide 24
     int leftSize = midIndex - leftIndex + 1;
     int rightSize = rightIndex - midIndex;
@@ -25,10 +25,10 @@ void merge(int arr[], int leftIndex, int midIndex, int rightIndex) {
     int leftHalf[leftSize];
     int rightHalf[rightSize];
     for (int i = 0; i < leftSize; i++) {
-        leftHalf[i] = arr[leftIndex + i];
+        leftHalf[i] = arr[leftIndex + i].getRating();
     }
     for (int i = 0; i < rightSize; i++) {
-        rightHalf[i] = arr[midIndex + i + 1];
+        rightHalf[i] = arr[midIndex + i + 1].getRating();
     }
 
     int leftCt = 0;
@@ -37,15 +37,15 @@ void merge(int arr[], int leftIndex, int midIndex, int rightIndex) {
 
     while (leftCt < leftSize && rightCt < rightSize) {
         if (leftHalf[leftCt] <= rightHalf[rightCt]) {
-            arr[mergedCt++] = leftHalf[leftCt++];
+            arr[mergedCt++].setRating(leftHalf[leftCt++]);
         } else {
-            arr[mergedCt++] = rightHalf[rightCt++];
+            arr[mergedCt++].setRating(rightHalf[rightCt++]);
         }
     }
     while (leftCt < leftSize) {
-        arr[mergedCt++] = leftHalf[leftCt++];
+        arr[mergedCt++].setRating(leftHalf[leftCt++]);
     }
     while (rightCt < rightSize) {
-        arr[mergedCt++] = rightHalf[rightCt++];
+        arr[mergedCt++].setRating(rightHalf[rightCt++]);
     }
 }
